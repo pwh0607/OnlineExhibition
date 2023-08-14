932d4ad8e332e131a35dab72f35510b9d41e6038
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2023-08-14 23:11:44 +0900

    스크립트 기능 추가 커밋
    닉네임 유효성 검사 기능 추가

--- Final_Project/Assets/Script/GameManager.cs
+++ Final_Project/Assets/Script/GameManager.cs
@@ -28,4 +28,17 @@ public class GameManager : MonoBehaviourPunCallbacks
         Debug.Log(PhotonNetwork.LocalPlayer.NickName+" : 로비 참가 성공!");
         PhotonNetwork.LoadLevel("Lobby");
     }
+
+    //유효성 검사
+    public void CheckName()
+    {
+        if (nickNameField.text.Length > 10 || nickNameField.text.Length < 2)       //10글자가 넘는 다면...
+        {
+            Debug.Log(nickNameField.text.Length + "글자수는 10자 미만 2글자 이상으로 해주세요.");
+        }
+        else
+        {       //조건 성립시 커넥트
+            Connect();
+        }
+    }
 }
