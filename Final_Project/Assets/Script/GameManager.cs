01db9443aebfbf7fb1db5b6f22986aa78e740c6d
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2023-08-16 17:29:10 +0900

    스크립트 수정 커밋
    로그인 씬 경고문 활성화 기능 추가

--- Final_Project/Assets/Script/GameManager.cs
+++ Final_Project/Assets/Script/GameManager.cs
@@ -10,8 +10,13 @@ public class GameManager : MonoBehaviourPunCallbacks
 {
     private string userNickname;
     public InputField nickNameField;
+    public GameObject warnMsg;
     public Button startBtn;
 
+    private void Start()
+    {
+        warnMsg.SetActive(false);
+    }
     //서버 연결
     public void Connect()
     {
@@ -28,13 +33,18 @@ public class GameManager : MonoBehaviourPunCallbacks
         Debug.Log(PhotonNetwork.LocalPlayer.NickName+" : 로비 참가 성공!");
         PhotonNetwork.LoadLevel("Lobby");
     }
-
+    private void setActivation()
+    {
+        warnMsg.SetActive(!warnMsg.active);
+    }
     //유효성 검사
     public void CheckName()
     {
         if (nickNameField.text.Length > 10 || nickNameField.text.Length < 2)       //10글자가 넘는 다면...
         {
             Debug.Log(nickNameField.text.Length + "글자수는 10자 미만 2글자 이상으로 해주세요.");
+            setActivation();
+            Invoke("setActivation", 2.0f);
         }
         else
         {       //조건 성립시 커넥트
