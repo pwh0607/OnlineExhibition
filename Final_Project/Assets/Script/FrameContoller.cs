c3ae3235c6856af6b47999e0324b70028dc903e1
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2023-09-23 20:44:12 +0900

    스크립트 삭제 커밋
    스크립트 삭제

--- Final_Project/Assets/Script/FrameContoller.cs
+++ Final_Project/Assets/Script/FrameContoller.cs
@@ -5,37 +5,11 @@ using Photon.Pun;
 
 public class FrameContoller : MonoBehaviour
 {
-   // public Camera cam;
     public Camera cam;
     private GameObject roomManager;
     private void Start()
     {
         roomManager = GameObject.Find("RoomManager");
         cam = GameObject.Find("mainCam").GetComponent<Camera>();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        SetImg();
-    }
-
-
-    void SetImg()
-    {
-        if (PhotonNetwork.IsMasterClient)       //방장인 경우
-        {
-            if (Input.GetMouseButtonDown(0) && roomManager.GetComponent<RoomManager>().GetMode() == 0)      //기본 모드 이고...
-            {
-                RaycastHit hit;
-                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-                {
-                    if (hit.transform.gameObject.tag == "Frame")
-                    {
-                        GetComponent<FileBrowser>().OnClickImageLoad();
-                    }
-                }
-            }
-        }
     }
 }
