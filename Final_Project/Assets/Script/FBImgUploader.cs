bba478e61912fe7928eed21875e9a84dd869ce38
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2023-11-15 18:46:57 +0900

    스크립트 수정 커밋 (2)
    FB storage에 이미지 업로드 기능 추가

--- Final_Project/Assets/Script/FBImgUploader.cs
+++ Final_Project/Assets/Script/FBImgUploader.cs
@@ -7,45 +7,16 @@ using System;
 using Firebase;
 using Firebase.Extensions;
 using Firebase.Storage;
+using Firebase.Database;
 
 public class FBImgUploader : MonoBehaviour
 {
-    RawImage rawImage;
-    FirebaseStorage storage;
-    StorageReference storageReference;
+    //FB 참조
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private DatabaseReference databaseRef;
 
-    IEnumerator imageLoad(string MediaUrl)
-    {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
-        yield return request.SendWebRequest();
-        if (request.isNetworkError || request.isHttpError)
-        {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            rawImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
-        }
-    }
+    string roomNumber;          //FB storage 폴더 접근용.
+    public GameObject cube;     //이미지
 
-    void Start()
-    {
-        rawImage = gameObject.GetComponent<RawImage>();
-       
-        //initialize storage reference
-        storage = FirebaseStorage.DefaultInstance;
-        storageReference = storage.GetReferenceFromUrl("gs://onlineexhibition-9dbef.appspot.com/");
-
-        //get reference of image
-        StorageReference image = storageReference.Child("testPhoto1.jpg");
-
-        //get the download link of file
-        image.GetDownloadUrlAsync().ContinueWithOnMainThread(task =>
-        {
-            if (!task.IsFaulted && !task.IsCanceled)
-            {
-                StartCoroutine(imageLoad(Convert.ToString(task.Result)));
-            }
-        });
-    }
 }
