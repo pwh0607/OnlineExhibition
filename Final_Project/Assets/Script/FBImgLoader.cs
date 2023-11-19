ede912b7a0afec052ae1bd80cca5a5f72b18fdc0
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2023-11-19 17:34:05 +0900

    스크립트 수정 커밋 (1)
    - 미사용 코드 삭제
    - 스토리지 및 DB 연동 기능 추가

--- Final_Project/Assets/Script/FBImgLoader.cs
+++ Final_Project/Assets/Script/FBImgLoader.cs
@@ -16,7 +16,7 @@ public class FBImgLoader : MonoBehaviour
     private StorageReference storageRef;
     private DatabaseReference databaseRef;
 
-    string roomNumber;          //FB storage 폴더 접근용.
+    string roomName;          //FB storage 폴더 접근용.
     public GameObject cube;     //이미지
 
     //이미지 매핑
@@ -39,17 +39,18 @@ public class FBImgLoader : MonoBehaviour
     void Start()
     {
         //변수값 초기화
-        roomNumber = "Room1";           //나중에 scene이름으로 변경.
+        roomName = "Room1";           //나중에 scene이름으로 변경.
 
         //데이터베이스 참조
         FirebaseApp.DefaultInstance.Options.DatabaseUrl = new System.Uri("https://onlineexhibition-9dbef-default-rtdb.firebaseio.com/");
-        databaseRef = FirebaseDatabase.DefaultInstance.RootReference.Child("rooms").Child(roomNumber);
+        databaseRef = FirebaseDatabase.DefaultInstance.RootReference.Child("rooms").Child(roomName).Child(gameObject.name);
 
         //스토리지 참조
         storage = FirebaseStorage.DefaultInstance;
-        storageRef = storage.GetReferenceFromUrl("gs://onlineexhibition-9dbef.appspot.com/Room1");
+        storageRef = storage.GetReferenceFromUrl("gs://onlineexhibition-9dbef.appspot.com").Child(roomName);
 
         //시작과 동시에 DB를 읽어서 오브젝트에 사진 매핑.
+        ReadDB();
     }
     
     //스토리지에서 이미지 가져오기
@@ -69,9 +70,11 @@ public class FBImgLoader : MonoBehaviour
     }
 
     //데이터베이스에서 사진path 가져오기
-    public void ReadDB(string _dataSet)
+    public void ReadDB()
     {
         // 특정 데이터셋의 DB 참조 얻기
+
+
         databaseRef.GetValueAsync().ContinueWith(
              task =>
              {
@@ -84,10 +87,10 @@ public class FBImgLoader : MonoBehaviour
                     Debug.Log("Load Data Success!");
                     DataSnapshot data = task.Result;       //rooms의 자식객체인 room1을 참조.
 
-                        // JSON 자체가 딕셔너리 기반   key는 오브젝트 이름(frame(n)) 값이 사진path;
+                    // JSON 자체가 딕셔너리 기반   key는 오브젝트 이름(frame(n)) 값이 사진path;
                     IDictionary dirInfo = (IDictionary)data.Value;
-                    Debug.Log("photo 1 : " + dirInfo[gameObject.name]);
-                    string photoPath = Convert.ToString(dirInfo[gameObject.name]);
+                    Debug.Log("photo 1 : " + dirInfo["photoPath"]);
+                    string photoPath = Convert.ToString(dirInfo["photoPath"]);
                     GetImage(photoPath);
                  }
              }
--- Final_Project/Assets/Script/FBImgUploader.cs
+++ Final_Project/Assets/Script/FBImgUploader.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Networking;
-using System;
+using UnityEditor;
+using System.IO;
+using Photon.Pun;
 using Firebase;
 using Firebase.Extensions;
 using Firebase.Storage;
@@ -12,6 +13,7 @@ using Firebase.Database;
 public class FBImgUploader : MonoBehaviour
 {
     //FB 참조
+
     private FirebaseStorage storage;
     private StorageReference storageRef;
     private DatabaseReference databaseRef;
@@ -36,19 +38,70 @@ public class FBImgUploader : MonoBehaviour
     {
         FirebaseApp.DefaultInstance.Options.DatabaseUrl = new System.Uri("https://onlineexhibition-9dbef-default-rtdb.firebaseio.com/");
         databaseRef = FirebaseDatabase.DefaultInstance.RootReference;
+
+        storage = FirebaseStorage.DefaultInstance;
+        storageRef = storage.GetReferenceFromUrl("gs://onlineexhibition-9dbef.appspot.com");
     }
 
-    public void DBUpload()
+    //데이터 베이스 업로드
+    public void DBUpload(string roomName, string imageFullName)
     {
         string objName = gameObject.name.Trim();
-        string photoPath = "obj1.jpg".Trim();
+        string photoPath = imageFullName.Trim();
 
         var data = new Photo(photoPath);
         string json = JsonUtility.ToJson(data);
 
-        databaseRef.Child("rooms").Child("room1").Child(objName).SetRawJsonValueAsync(json);
+        databaseRef.Child("rooms").Child(roomName).Child(objName).SetRawJsonValueAsync(json);
 
         Debug.Log(json);
         Debug.Log("DB 저장 완료!");
+
+        //저장후 씬 오브젝트에 이미지 update 
+        gameObject.GetComponent<FBImgLoader>().ReadDB();
+    }
+
+    //이미지 업로드
+    public void OnClickImageLoad()
+    {
+        NativeGallery.GetImageFromGallery((file) => {
+            FileInfo img = new FileInfo(file);
+
+            if (!string.IsNullOrEmpty(file))
+            {
+                //불러오기
+                StartCoroutine(LoadImage(file));
+            }
+        });
+
+        IEnumerator LoadImage(string imagePath)
+        {
+            byte[] imageData = File.ReadAllBytes(imagePath);
+            string imageName = Path.GetFileName(imagePath).Split('.')[0];
+            string saveImagePath = Application.persistentDataPath + "/Image";
+            string imageFullName = imageName + ".jpg";
+
+            File.WriteAllBytes(saveImagePath + imageFullName, imageData);
+            
+            //metadata세팅
+            var metaData = new MetadataChange();
+            metaData.ContentType = "image/jpg";
+
+            StorageReference uploadRef = storageRef.Child("Room1/" + imageFullName);        //수정 필요!
+            uploadRef.PutBytesAsync(imageData).ContinueWithOnMainThread((task) =>
+            {
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    Debug.Log("실패...");
+                }
+                else
+                {
+                    Debug.Log("성공!!");
+                }
+            });
+
+            DBUpload("Room1", imageFullName);
+            yield return null;
+        }
     }
 }
